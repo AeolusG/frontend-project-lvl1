ff951baabeb81f62db146d83d3e754a6f2a09cfd
Olga Komarova <Olga68ko@gmail.com>
2021-11-06 15:07:50 +0300

    rewrite brainCalc

--- src/brainCalc.js
+++ src/brainCalc.js
@@ -11,13 +11,8 @@ const brainCalc = () => {
   const randomNumber = randomNum(100, 0);
   const randomNumber2 = randomNum(100, 0);
   const pair = cons(randomNumber, randomNumber2);
-
   const operations = ['+', '-', '*'];
   const expressions = [car(pair) + cdr(pair), car(pair) - cdr(pair), car(pair) * cdr(pair)];
-  for (let i = 0; i <= 3; i += 1) {
-    const question = `${car(pair)} ${operations[i]} ${cdr(pair)}`;
-    const currentAnswer = `${expressions[i]}`;
-  }
   return [question, currentAnswer];
 };
 
