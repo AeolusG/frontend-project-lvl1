b74b3108ed67599f25a63302b30b42fdc9413851
Olga Komarova <Olga68ko@gmail.com>
2021-11-06 18:49:58 +0300

    подвела игру brain-calc под общую логику

--- src/brainCalc.js
+++ src/brainCalc.js
@@ -1,18 +1,29 @@
 #!/usr/bin/env node
 
-import {
-  cons, car, cdr,
-} from '@hexlet/pairs';
+import { cons, car, cdr } from '@hexlet/pairs';
 import { randomNum, gameLogic } from '../src/index.js';
 
 const description = 'What is the result of the expression?';
+
+let i = null;
+
+const randomOperation = () => {
+  i = randomNum(2, 0);
+  const operation = ['+', '-', '*'];
+  return operation[i];
+};
 
 const brainCalc = () => {
   const randomNumber = randomNum(100, 0);
   const randomNumber2 = randomNum(100, 0);
   const pair = cons(randomNumber, randomNumber2);
-  const operations = ['+', '-', '*'];
-  const expressions = [car(pair) + cdr(pair), car(pair) - cdr(pair), car(pair) * cdr(pair)];
+  const expressions = [
+    car(pair) + cdr(pair),
+    car(pair) - cdr(pair),
+    car(pair) * cdr(pair),
+  ];
+  const question = `${car(pair)} ${randomOperation()} ${cdr(pair)}`;
+  const currentAnswer = `${expressions[i]}`;
   return [question, currentAnswer];
 };
 
--- src/isEven.js
+++ src/isEven.js
@@ -2,11 +2,12 @@
 
 import { randomNum, gameLogic } from '../src/index.js';
 
-const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+const description = 'Answer "yes" if the number is even, otherwise answer "no"';
 
 const isEven = () => {
   const question = randomNum(50, 0);
   const currentAnswer = question % 2 === 0 ? 'yes' : 'no';
+
   return [question, currentAnswer];
 };
 
