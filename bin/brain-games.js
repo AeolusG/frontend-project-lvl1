822ba93bdaa8ff574a9f7af143b941d6e441226a
Olga Komarova <Olga68ko@gmail.com>
2021-11-09 14:16:04 +0300

    Добавила brain-games

---
+++ bin/brain-games.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable import/no-named-as-default */
+
+import greet from '../src/games/games.js';
+
+greet();
